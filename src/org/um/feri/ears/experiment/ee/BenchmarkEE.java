7eff18e58111009359279037fcc2da1c18b77571
Ravby <ravby69@gmail.com>
2020-12-23 20:32:10 +0100

    Refactoring and removal of unused examples

--- src/org/um/feri/ears/experiment/ee/BenchmarkEE.java
+++ src/org/um/feri/ears/experiment/ee/BenchmarkEE.java
@@ -65,7 +65,7 @@ public class BenchmarkEE extends RatingBenchmark {
         if ((a==null) &&(b==null)) return true;
         if (a==null) return false;
         if (b==null) return false;
-        if (Math.abs(a.getEval()-b.getEval())<draw_limit) return true;
+        if (Math.abs(a.getEval()-b.getEval())< drawLimit) return true;
         return false;
     }
     public BenchmarkEE() {
@@ -79,7 +79,7 @@ public class BenchmarkEE extends RatingBenchmark {
         initFullProblemList();
         addParameter(EnumBenchmarkInfoParameters.DIMENSION,String.valueOf(D));
         addParameter(EnumBenchmarkInfoParameters.EVAL,String.valueOf(maxEvaluations));
-        addParameter(EnumBenchmarkInfoParameters.DRAW_PARAM,"abs(evaluation_diff) < "+draw_limit);
+        addParameter(EnumBenchmarkInfoParameters.DRAW_PARAM,"abs(evaluation_diff) < "+ drawLimit);
     }
     /* (non-Javadoc)
      * @see org.um.feri.ears.benchmark.RatingBenchmark#registerTask(org.um.feri.ears.problems.Problem)
