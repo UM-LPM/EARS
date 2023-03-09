5055df479e3effd01214ae304cbf4cbfe6ed746b
Ravby <miha.ravber@gmail.com>
2023-03-09 13:24:32 +0100

    Resolving merge conflict

--- src/org/um/feri/ears/problems/unconstrained/SchmidtVetters.java
+++ src/org/um/feri/ears/problems/unconstrained/SchmidtVetters.java
@@ -1,5 +1,6 @@
 package org.um.feri.ears.problems.unconstrained;
 
+import org.um.feri.ears.problems.DoubleProblem;
 import org.um.feri.ears.problems.Problem;
 
 import java.util.ArrayList;
@@ -10,16 +11,16 @@ import static java.lang.Math.*;
 https://al-roomi.org/benchmarks/unconstrained/3-dimensions/129-schmidt-vetters-function
 http://infinity77.net/global_optimization/test_functions_nd_S.html#go_benchmark.SchmidtVetters
  */
-public class SchmidtVetters extends Problem {
+public class SchmidtVetters extends DoubleProblem {
     public SchmidtVetters() {
-        super(3, 0, 1);
+        super("SchmidtVetters",3, 1, 1, 0);
         lowerLimit = new ArrayList<>(Collections.nCopies(numberOfDimensions, 0.0));
         upperLimit = new ArrayList<>(Collections.nCopies(numberOfDimensions, 10.0));
         name = "SchmidtVetters";
 
-        optimum[0][0] = 7.0708;
-        optimum[0][1] = 10.0;
-        optimum[0][2] = 3.1416;
+        decisionSpaceOptima[0] = new double[]{7.0708, 10.0, 3.1416};
+
+        objectiveSpaceOptima[0] = -0.19397252244022722;
     }
 
     @Override
@@ -28,10 +29,5 @@ public class SchmidtVetters extends Problem {
         double aux2 = sin((Math.PI * x[1] + x[2]) / 2);
         double aux3 = exp(pow((x[0] + x[1]) / (x[1]) - 2, 2));
         return aux1 + aux2 + aux3;
-    }
-
-    @Override
-    public double getGlobalOptimum() {
-        return 0.19397252244022722;
     }
 }
